f2bbdb7c4a9d2a35cf06b117997979c3b4b81740
Daniel Carlander <carlander.daniel@gmail.com>
2018-09-18 11:20:19 +0200

    Initial Commit

---
+++ Cubo Flotando/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cubo Flotando
+//
+//  Created by Daniel Carlander on 18/09/2018.
+//  Copyright © 2018 Daniel Carlander. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
